6eede951619273f5f4d7d92c2cb1615e71451707
eric mead <ermead@vt.edu>
2015-11-18 15:34:31 -0700

    wired up mock data to table views

---
+++ Pair Randomizer/OtherListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  OtherListViewController.swift
+//  Pair Randomizer
+//
+//  Created by Eric Mead on 11/18/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import UIKit
+
+class OtherListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("otherCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "test"
+        cell.imageView?.image = UIImage(named: "default")
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return OtherController.sharedController.others.count
+        
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pair Randomizer/PeopleListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PeopleListViewController.swift
+//  Pair Randomizer
+//
+//  Created by Eric Mead on 11/18/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import UIKit
+
+class PeopleListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("peopleCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "test"
+        cell.imageView?.image = UIImage(named: "default")
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return PeopleController.sharedController.persons.count
+        
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
